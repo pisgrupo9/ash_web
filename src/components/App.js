35987b2f1a5b4d64028242440445d9d7226ec3f0
Joaquin Lejtreger <joaclej@gmail.com>
2016-09-01 16:15:54 -0300

    Se crea componente Footer y se agregar a App.js ademas, se modifica LogoHeader para que tomo como parametro un string y no un objeto.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 // This component handles the App template used on every page.
 import React, { Component, PropTypes } from 'react';
+import Footer from './common/Footer.js';
 
 const { object } = PropTypes;
 
 class App extends Component {
   render() {
+    const footerText = 'Animales Sin Hogar - Asociación Civil sin fines de lucro. - Personería Jurídica 9985';
     return (
       <div>
         {this.props.children}
+        <Footer text={footerText} />
       </div>
     );
   }
