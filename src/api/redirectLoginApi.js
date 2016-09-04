97c0fb1dee6286e34b1a417631769689de1bea2a
Joaquin Lejtreger <joaclej@gmail.com>
2016-09-04 16:23:02 -0300

    Se agregan funciones para redirigir en cas ode que usuario intente acceder a donde no debe

---
+++ src/api/redirectLoginApi.js
@@ -0,0 +1,15 @@
+import * as session from '../actions/stateActions';
+
+export const CheckAuth  = (nextState, replace) => {
+  if (!isLogged()) {
+   replace({
+     pathname: '/login',
+     state: { nextPathname: nextState.location.pathname }
+   });
+  }
+};
+
+export const isLogged = () => {
+  let current_session = session.loadState();
+  return(current_session && current_session.user && current_session.user.token);
+};
