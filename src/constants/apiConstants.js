f3cd86a1f3f0000e8ed413f68039339fb96bdfac
David <gun.woo.lee@fing.edu.uy>
2016-09-05 17:02:22 -0300

    logout&Heroku:logout&Heroku

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,4 +1,5 @@
 export const APIARY_URL = 'http://private-205612-ashweb.apiary-mock.com';
 export const API_STAGING_URL = 'http://ash-api-staging.herokuapp.com';
+export const API_URL = 'https://ash-api.herokuapp.com/api/v1';
 export const HEROKU_URL = 'https://ash-api.herokuapp.com';
 export const SECCION_STORAGE = 'ASH-SECCION-TOKEN';
