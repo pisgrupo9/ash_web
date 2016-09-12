9983b3eda7ca047826eb581e85a70ebaf3be2e02
Bernabe <bernabe@toptierlabs.com>
2016-09-12 17:26:08 -0300

    se configuro redux immutable state invariant

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -5,10 +5,12 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
   const middewares = [
     // Add other middleware on this line...
+    reduxImmutableStateInvariant(),
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
