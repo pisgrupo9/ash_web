9675e1ebe7e183bf5b7e63f332ca44f5745e913e
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-10-09 13:12:54 -0300

    stick filter to left

--- src/containers/AnimalListPage.js
+++ src/containers/AnimalListPage.js
@@ -2,18 +2,21 @@ import React from 'react';
 import AnimalListWrapper from '../components/animals/AnimalListWrapper';
 import '../styles/animal-list.scss';
 import AnimalFilters from '../components/animals/AnimalFilters';
+import { StickyContainer, Sticky } from 'react-sticky';
 
 const AnimalListPage = () => {
-    return (
-      <div className="list-flex">
-        <div className="filter-section">
+  return (
+    <div className="list-flex">
+      <StickyContainer className="filter-section">
+        <Sticky>
           <AnimalFilters/>
-        </div>
-        <div className="wrapper-flex">
-          <AnimalListWrapper />
-        </div>
+        </Sticky>
+      </StickyContainer>
+      <div className="wrapper-flex">
+        <AnimalListWrapper />
       </div>
-    );
+    </div>
+  );
 };
 
 export default AnimalListPage;
