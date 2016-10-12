f1e5a54af00fe7048dba3deba121b331b15c8374
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-10-12 20:26:39 -0300

    se arreglaron cosas de diseño

--- src/containers/AnimalListPage.js
+++ src/containers/AnimalListPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import AnimalListWrapper from '../components/animals/AnimalListWrapper';
 import '../styles/animal-list.scss';
 import AnimalFilters from '../components/animals/AnimalFilters';
-import { StickyContainer, Sticky } from 'react-sticky';
+import { StickyContainer } from 'react-sticky';
+import StickyResponsive from '../components/common/StickyResponsive';
 
 const AnimalListPage = () => {
   return (
     <div className="list-flex">
       <StickyContainer className="filter-section">
-        <Sticky>
+        <StickyResponsive>
           <AnimalFilters/>
-        </Sticky>
+        </StickyResponsive>
       </StickyContainer>
       <div className="wrapper-flex">
         <AnimalListWrapper />
