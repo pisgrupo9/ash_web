af51bd5c9036a3bf3bca8938803420d8d185ae91
Joaquin Lejtreger <joaclej@gmail.com>
2016-10-15 19:19:24 -0300

    bug de nombre largo de evento y estilo reportes

--- src/components/reports/ReportList.js
+++ src/components/reports/ReportList.js
@@ -5,11 +5,11 @@ const ReportList = ({ reports }) => {
 
   return (
      <div className="report-table" >
-      <div className="report-titles-container">
-        <div className="report-title-inside">FECHA</div>
-        <div className="report-title-inside">NOMBRE</div>
-        <div className="report-title-inside-type">TIPO</div>
-        <div className="report-title-inside">ESTADO</div>
+      <div className="report-titles-container title">
+        <div className="report-inside">FECHA</div>
+        <div className="report-inside">NOMBRE</div>
+        <div className="report-inside-type">TIPO</div>
+        <div className="report-inside">ESTADO</div>
       </div>
       { reports.map(report => {
           return (<ReportItem report={report} key={report.fecha}/>);
