25a9717d6393b17899009241b02c290d6b27b57e
Joaquin Lejtreger <joaclej@gmail.com>
2016-10-26 18:58:22 -0300

    libreria + container de estadisticas

--- src/containers/EstadisticasPage.js
+++ src/containers/EstadisticasPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../styles/statistics.scss';
 
 class EstadisticasPage extends Component {
   constructor(props, context) {
@@ -8,8 +9,18 @@ class EstadisticasPage extends Component {
 
   render() {
     return (
-      <div>
-        <p>Proximamente Estadisticas</p>
+      <div className="profile-page-flex">
+        <div className="outer-flex">
+          <div className="statistic-div inner-flex">
+            ESTADISTICA 1
+          </div>
+          <div className="statistic-div inner-flex">
+            ESTADISTICA 3
+          </div>
+        </div>
+        <div className="inner-flex statistic-div">
+          ESTADISTICA 3
+        </div>
       </div>
     );
   }
