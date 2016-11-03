30607d17054224157f41f58996c8a436ad771f79
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-11-03 20:48:03 -0300

    se mejoro la vista de info adoptante

--- src/components/adopters/perfil/AdopterInfo.js
+++ src/components/adopters/perfil/AdopterInfo.js
@@ -11,39 +11,33 @@ const AdopterInfo = ({ adopter, loading, styleClass, loadingFunc, adopterId }) =
       <div>
         <StickyResponsive>
           <div>
-            <div className="center tabel-div adopter">
-              <table className="table-borderless">
-                <tbody>
-                 <tr>
-                    <td><b>NOMBRE:</b></td>
-                    <td><b>{`${adopter.first_name} ${adopter.last_name}`}</b></td>
-                  </tr>
-                  <tr>
-                    <td>CI:</td>
-                    <td>{adopter.ci}</td>
-                  </tr>
-                  <tr>
-                    <td>EMAIL:</td>
-                    <td>{adopter.email ? adopter.email : '-'}</td>
-                  </tr>
-                  <tr>
-                    <td>TELEFONO:</td>
-                    <td>{adopter.phone ? adopter.phone : '-'}</td>
-                  </tr>
-                  <tr>
-                    <td>DIRECCIÓN:</td>
-                    <td>{adopter.home_address ? adopter.home_address : '-'}</td>
-                  </tr>
-                  <tr>
-                    <td>LISTA NEGRA:</td>
-                    <td>{adopter.blacklisted ? <b> SI </b> : 'NO'}</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="adopter-info-row">
+              <p className="row-title"><b>NOMBRE:</b></p>
+              <p><b>{`${adopter.first_name} ${adopter.last_name}`}</b></p>
+            </div>
+            <div className="adopter-info-row">
+              <p className="row-title">CI:</p>
+              <p>{adopter.ci}</p>
+            </div>
+            <div className="adopter-info-row">
+              <p className="row-title">EMAIL:</p>
+              <p>{adopter.email ? adopter.email : '-'}</p>
+            </div>
+            <div className="adopter-info-row">
+              <p className="row-title">TELEFONO:</p>
+              <p>{adopter.phone ? adopter.phone : '-'}</p>
+            </div>
+            <div className="adopter-info-row">
+              <p className="row-title">DIRECCIÓN:</p>
+              <p>{adopter.home_address ? adopter.home_address : '-'}</p>
+            </div>
+            <div className="adopter-info-row">
+              <p className="row-title">LISTA NEGRA:</p>
+              <p>{adopter.blacklisted ? <b> SI </b> : 'NO'}</p>
             </div>
             <div className="house-des">
               <p className="black-color">DESCRIPCIÓN CASA</p>
-              {adopter.house_description}
+              <p>{adopter.house_description}</p>
             </div>
             <div className="acction-button">
               <EditAdopterButton adopter={adopter} loading={loadingFunc} adopterId={adopterId}/>
