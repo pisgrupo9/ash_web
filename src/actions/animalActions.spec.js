a3a6244d9b0477937f19812cdc43f774a4a438f8
Isa14 <isabel@neocoast.com>
2016-11-06 00:05:42 -0300

    Tests para el formulario de un animal

---
+++ src/actions/animalActions.spec.js
@@ -0,0 +1,213 @@
+import { expect } from 'chai';
+import * as animalActions from '../actions/animalActions';
+import * as types from '../actions/actionTypes';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import { parseAnimal } from '../api/animalApi';
+import * as consts from '../constants/apiConstants.js';
+import { createStore } from 'redux';
+import initialState from '../reducers/initialState';
+import rootReducer from '../reducers';
+
+describe('Animal Actions', () => {
+  describe('Crear un animal', () => {
+
+    it('devuelve la acción sendAnimalFormSuccess', () => {
+      const response = 'Nuevo animal';
+      const expectedAction = { type: types.SEND_ANIMAL_FORM_SUCCESS, response };
+      const action = animalActions.sendAnimalFormSuccess(response);
+
+      expect(action).to.deep.equal(expectedAction);
+    });
+
+    it('devuelve la acción sendAnimalFormError', () => {
+      const errors = 'No se pudo crear';
+      const expectedAction = { type: types.SEND_ANIMAL_FORM_ERROR, errors };
+      const action = animalActions.sendAnimalFormError(errors);
+
+      expect(action).to.deep.equal(expectedAction);
+    });
+
+    it('devuelve la acción cancelAnimalForm', () => {
+      const expectedAction = { type: types.CANCEL_ANIMAL_FORM };
+      const action = animalActions.cancelAnimalForm();
+
+      expect(action).to.deep.equal(expectedAction);
+    });
+  });
+
+  const middleware = [thunk];
+  const mockStore = configureMockStore(middleware);
+
+  describe('Enviar el formulario de un animal con acciones asíncronas', () => {
+    afterEach(() => {
+      nock.cleanAll();
+    });
+
+    describe('Exito al enviar el formulario del animal', () => {
+      const animal = {
+        chip_num: '1',
+        species_id: '1',
+        sex: 'female',
+        admission_date: '2010-10-10',
+        name: 'Test',
+        birthdate: '2010-10',
+        race: 'beagle',
+        death_date: '',
+        castrated: false,
+        vaccines: false,
+        profile_image: 'image_data',
+        weight: 15
+      };
+      nock(consts.API_STAGING_URL)
+        .post('/animals', parseAnimal(animal))
+        .reply(201, { id: 43 });
+
+      it('Devuelve la acción sendAnimalFormSuccess con sendAnimalForm', () => {
+        const expectedAction = [{
+          type: 'SEND_ANIMAL_FORM_SUCCESS', response: { id: 43 }
+        }];
+
+        const store = mockStore(initialState.animalForm);
+
+        return store.dispatch(animalActions.sendAnimalForm(animal))
+          .then(() => {
+            expect(store.getActions()).to.deep.equal(expectedAction);
+          });
+      });
+
+      it('La acción sendAnimalFormSuccess cambia a success en el store', () => {
+        const expectedAction = {
+          type: 'SEND_ANIMAL_FORM_SUCCESS', response: { id: 43 }
+        };
+
+        const store = createStore(rootReducer, initialState);
+
+        store.dispatch(expectedAction);
+        expect(store.getState().animalForm.success).to.equal(true);
+      });
+    });
+
+    describe('Error al enviar el formulario del animal', () => {
+      it('Cuando el nombre es vacío', () => {
+        const animal = {
+          chip_num: '1',
+          species_id: '1',
+          sex: 'female',
+          admission_date: '2010-9-10',
+          name: '',
+          birthdate: '2010-10',
+          race: 'beagle',
+          death_date: '',
+          castrated: false,
+          vaccines: false,
+          profile_image: 'image_data',
+          weight: 15
+        };
+        nock(consts.API_STAGING_URL)
+          .post('/animals', parseAnimal(animal))
+          .reply(422, { error: { name: 'no puede estar en blanco' } });
+
+        it('Devuelve la acción sendAnimalFormError con sendAnimalForm cuando el nombre esta vacío', () => {
+          const store = mockStore(initialState.animalForm);
+          const errors = 'no puede estar en blanco';
+          const expectedAction = [{
+            type: types.SEND_ANIMAL_FORM_ERROR, errors: { error: { name: errors } }
+          }];
+
+          return store.dispatch(animalActions.sendAnimalForm(animal))
+            .then(() => {
+              expect(store.getActions()).to.deep.equal(expectedAction);
+            });
+        });
+
+        it('La acción sendAnimalFormError guarda en el store el error', () => {
+          const store = createStore(rootReducer, initialState);
+          const errors = 'no puede estar en blanco';
+          const expectedAction = {
+            type: types.SEND_ANIMAL_FORM_ERROR, errors: { error: { name: errors } }
+          };
+          const expectedError = { name: errors };
+
+          store.dispatch(expectedAction);
+          expect(store.getState().animalForm.errors).to.deep.equal(expectedError);
+        });
+      });
+      it('Cuando la fecha de nacimiento es posterior a la fecha de admisión', () => {
+        const animal = {
+          chip_num: '1',
+          species_id: '1',
+          sex: 'female',
+          admission_date: '2010-10-10',
+          name: 'Testito',
+          birthdate: '2011-10',
+          race: 'beagle',
+          death_date: '',
+          castrated: false,
+          vaccines: false,
+          profile_image: 'image_data',
+          weight: 15
+        };
+        const errors = {
+          birthdate: ['La fecha de nacimiento es inválida.'],
+          admission_date: ['La fecha de ingreso es inválida.']
+        };
+        nock(consts.API_STAGING_URL)
+          .post('/animals', parseAnimal(animal))
+          .reply(422, { error: errors });
+        it('Devuelve la acción sendAnimalFormError con sendAnimalForm', () => {
+          const store = mockStore(initialState.animalForm);
+          const expectedAction = [{
+            type: types.SEND_ANIMAL_FORM_ERROR, errors: { error: errors }
+          }];
+
+          return store.dispatch(animalActions.sendAnimalForm(animal))
+            .then(() => {
+              expect(store.getActions()).to.deep.equal(expectedAction);
+            });
+        });
+
+        it('La acción sendAnimalFormError guarda en el store el error', () => {
+          const store = createStore(rootReducer, initialState);
+          const expectedAction = {
+            type: types.SEND_ANIMAL_FORM_ERROR, errors: { error: errors }
+          };
+          const expectedError = errors;
+
+          store.dispatch(expectedAction);
+          expect(store.getState().animalForm.errors).to.deep.equal(expectedError);
+        });
+      });
+      it('Sin token', () => {
+        const animal = {
+          chip_num: '1',
+          species_id: '1',
+          sex: 'female',
+          admission_date: '2010-10-10',
+          name: '',
+          birthdate: '2010-10',
+          race: 'beagle',
+          death_date: '',
+          castrated: false,
+          vaccines: false,
+          profile_image: 'image_data',
+          weight: 15
+        };
+        nock(consts.API_STAGING_URL)
+          .post('/animals', parseAnimal(animal))
+          .reply(422, { error: 'Debe proveer un token válido' });
+        const store = mockStore(initialState.animalForm);
+        const errors = 'Debe proveer un token válido';
+        const expectedAction = [{
+          type: types.SEND_ANIMAL_FORM_ERROR, errors: { error: errors }
+        }];
+
+        return store.dispatch(animalActions.sendAnimalForm(animal))
+          .then(() => {
+            expect(store.getActions()).to.deep.equal(expectedAction);
+          });
+      });
+    });
+  });
+});
---
+++ src/components/animals/AnimalForm.spec.js
@@ -0,0 +1,93 @@
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import AnimalForm from './AnimalForm';
+import { Checkbox } from 'react-bootstrap';
+import ImagesDropzone from '../common/ImagesDropzone';
+import ProfileDropzone from './ProfileDropzone';
+import ModalAnimalButtons from '../common/ModalAnimalButtons';
+import SelectInput from '../common/SelectInput';
+import Input from '../common/Input';
+
+function setup(animal, onSave, onCancel) {
+  let props = {
+    animal,
+    species: [],
+    images: [],
+    profilePic: {},
+    onSave,
+    onChange: () => {},
+    onCancel,
+    onDrop: () => {},
+    onDelete: () => {},
+    onDropProfile: () => {},
+    errors: {}
+  };
+
+   return shallow(<AnimalForm {...props}/>);
+}
+
+describe('Elementos del formulario de crear un animal', () => {
+  it('exite el formulario', () => {
+    expect(AnimalForm).to.exist;
+  });
+
+  it('Existen los campos del formulario', () => {
+    const animal = { species_id: '' };
+    const onSave = () => {};
+    const onCancel = () => {};
+    const wrapper = setup(animal, onSave, onCancel);
+    expect(wrapper.find('div')).to.have.length(4);
+    expect(wrapper.find(Input)).to.have.length(7);
+    expect(wrapper.find(Input)['nodes'][0].props.name).to.equal('chip_num');
+    expect(wrapper.find(Input)['nodes'][1].props.name).to.equal('name');
+    expect(wrapper.find(Input)['nodes'][2].props.name).to.equal('weight');
+    expect(wrapper.find(Input)['nodes'][3].props.name).to.equal('admission_date');
+    expect(wrapper.find(Input)['nodes'][4].props.name).to.equal('race');
+    expect(wrapper.find(Input)['nodes'][5].props.name).to.equal('birthdate');
+    expect(wrapper.find(Input)['nodes'][6].props.name).to.equal('death_date');
+
+    expect(wrapper.find(SelectInput)).to.have.length(2);
+    expect(wrapper.find(SelectInput)['node'].props.name).to.equal('sex');
+    expect(wrapper.find(SelectInput).last()['node'].props.name).to.equal('species_id');
+
+    expect(wrapper.find(ProfileDropzone)).to.have.length(1);
+    expect(wrapper.find(ImagesDropzone)).to.have.length(1);
+  });
+
+  it('Cuando el animal es perro se muestran las opciones de vacunado y castrado', () => {
+    const animal = { species_id: '1' };
+    const onSave = () => {};
+    const onCancel = () => {};
+    const wrapper = setup(animal, onSave, onCancel);
+    expect(wrapper.find(Checkbox)['node'].props.name).to.equal('castrated');
+    expect(wrapper.find(Checkbox).last()['node'].props.name).to.equal('vaccines');
+  });
+
+  it('Cuando el animal es gato se muestran las opciones de vacunado y castrado', () => {
+    const animal = { species_id: '2' };
+    const onSave = () => {};
+    const onCancel = () => {};
+    const wrapper = setup(animal, onSave, onCancel);
+    expect(wrapper.find(Checkbox)['node'].props.name).to.equal('castrated');
+    expect(wrapper.find(Checkbox).last()['node'].props.name).to.equal('vaccines');
+  });
+
+  it('Cuando el animal es de otra especie no se muestran las Checkbox', () => {
+    const animal = { species_id: '3' };
+    const onSave = () => {};
+    const onCancel = () => {};
+    const wrapper = setup(animal, onSave, onCancel);
+    expect(wrapper.find(Checkbox).length).to.equal(0);
+  });
+
+  it('Donde están los botones dentro del formulario', () => {
+    const animal = {};
+    const onSave = sinon.spy();
+    const onCancel = sinon.spy();
+    const wrapper = setup(animal, onSave, onCancel);
+    expect(wrapper.find(ModalAnimalButtons)).to.have.length(1);
+    expect(wrapper.find(ModalAnimalButtons)['node'].props.title).to.equal('INGRESAR');
+  });
+});
