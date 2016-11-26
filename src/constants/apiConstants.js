dc2828249d846459f17186c377bcc59d99c84ed7
Bernabe Gonzalez <bernabe@toptierlabs.com>
2016-11-26 14:26:32 -0300

    fix safari y algunos arreglos produccion

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,7 +1,10 @@
 export const APIARY_URL = 'http://private-205612-ashweb.apiary-mock.com';
+/*export const API_STAGING_URL = 'http://api-ash.herokuapp.com/api/v1';
+export const API_STAGING = 'http://api-ash.herokuapp.com';
+export const STATIC_RESOURCE = 'https://s3.amazonaws.com/ash-web-assets';*/
 export const API_STAGING_URL = 'http://ash-api-staging.herokuapp.com/api/v1';
 export const API_STAGING = 'http://ash-api-staging.herokuapp.com';
-export const STATIC_RESOURCE = 'https://s3-sa-east-1.amazonaws.com';
+export const STATIC_RESOURCE = 'https://s3-sa-east-1.amazonaws.com/ash-assets';
 export const SECCION_STORAGE = 'ASH-SECCION-TOKEN';
 export const ANIMAL_PAGE_SIZE = 15;
 export const ADOPTER_PAGE_SIZE = 10;
